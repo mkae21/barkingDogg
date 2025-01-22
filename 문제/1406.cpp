d36864360480be3b9709018122ac40308ba320a7
mkae21 <bbuchu123@gmail.com>
2025-01-22 21:20:31 +0900

    Update

--- 문제/1406.cpp
+++ 문제/1406.cpp
@@ -2,10 +2,51 @@
 
 using namespace std;
 
+// void commandL(list<char>& list,list<char>::iterator &cursor){
+//     --cursor;
+// }
+
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
-
     
+    int N;
+
+    list<char> L;
+    string input;
+    
+    cin >> input;
+
+    for(auto c : input) //다른 초기화 방법?
+        L.push_back(c);
+    
+    //커서 초기화
+    list<char>::iterator cursor = L.end(); //cursor
+    cin >>  N; //명령어 갯수
+
+    for(int i = 0; i < N ; i++){
+        char temp;
+        cin >> temp;
+
+        if(temp == 'L'){
+            if(cursor != L.begin())
+                cursor--;
+        }else if(temp == 'D'){
+            if(cursor != L.end())
+                cursor++;
+        }else if(temp == 'B'){
+            if(cursor != L.begin()){
+                cursor = L.erase(--cursor);
+            }
+        }else{//추가
+            cin >> temp;
+            L.insert(cursor,temp);
+        }
+
+    }
+
+    for(auto c: L)
+        cout << c;
+
     return 0;
 }
--- 수업/prac.cpp
+++ 수업/prac.cpp
@@ -2,32 +2,27 @@
 
 using namespace std;
 
+
 int main(){
+    queue<int> q;
+    
+    q.push(10);
+    q.push(20);
+    q.push(30);
+    
+    cout << q.size() << '\n'; // 3 출력
 
-    list<int> L = {1,2}; // 연결리스트 선언
-    list<int>::iterator t = L.begin(); // t는 1을 가리키는 중
+    if(q.empty())
+        cout << "queue is empty" << '\n';
+    else
+        cout << "not empty" << '\n';
+    
+    cout << q.front() << '\n';
+    cout << q.back() << '\n';
 
-    L.push_front(10);  // 10 1 2
-    cout << *t << '\n';
+    q.pop();
 
-    L.push_back(5);// 10 1 2 5
-    L.insert(t,6);// t가 가리키는 곳 앞에 6을 삽입, 10 6 1 2 5
-    t++; //다음 원소
-
-    t = L.erase(t); // erase()는 t가 가리키는 값을 제거 후 그 다음 원소인 5의 위치를 반환
-                    // 10 6 1 5,t 가 가리키는 값은 5
-                    // 링크드 리스트의 삭제와 동일하다
-
-    cout << *t << '\n';
-
-    for(auto i:L) // cpp 11 이상
-        cout << i <<' ';
-
-    cout << '\n';
-
-    for(list<int>::iterator it = L.begin();it != L.end();it++){ //cpp 11 미만
-        cout << *it << ' ';
-    }
+    cout << q.front() << '\n';
 
     return 0;
 }
