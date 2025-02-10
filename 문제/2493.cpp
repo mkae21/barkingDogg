27d8869be879c29ee84cc1203d66614c89408bc0
mkae21 <bbuchu123@gmail.com>
2025-02-10 19:35:15 +0900

    Update 2493.cpp

--- 문제/2493.cpp
+++ 문제/2493.cpp
@@ -2,30 +2,29 @@
 
 using namespace std;
 
-//시간 제한: 1.5초 N: 500,000
-
-int dis[100000001]; // 높이 배열
-
 int main(){
-
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    stack<int> s; // 입력 저장
-    stack<int> ans; //정답 저장
-    queue<int> temp; //임시 저장
-    
-    int n,input;
-    int idx = 0;
+    int n;
+    stack<pair<int,int>> tower;
+    tower.push({100000001,0}); //가장 큰 타워
+
     cin >> n;
 
-    while(n--){ //초기화
-        cin >> input;
-        dis[input] = idx++;
-        s.push(input);
+    for(int i = 1; i <= n ; i++){
+        int height;
+        cin >> height;
+
+        while(tower.top().first < height){
+            tower.pop();
+        }
+
+        cout << tower.top().second << " ";
+
+        tower.push({height,i});
+
     }
-
-
 
 
     return 0;
